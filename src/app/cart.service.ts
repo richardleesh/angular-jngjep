6e93122dfa20b2b9ef3d740bed2cd22f898af484
richardleesh <richardleesh@gmail.com>
2021-03-13 12:23:09 -0500

    3 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -20,7 +20,7 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+   return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
   }
 
   constructor(
